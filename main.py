aad6f4ead0fa7aea6d0e1b322edb93ed51c44135
hhalst <hhalst@uw.edu>
2026-07-07 14:23:10 -0700

    Update main.py
    put in my thowaway email and python email app password

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ import smtplib
 import os
 
 # import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+# MY_EMAIL = os.environ.get("MY_EMAIL")
+# MY_PASSWORD = os.environ.get("MY_PASSWORD")
+MY_EMAIL = "hannie.throwaway@gmail.com"
+MY_PASSWORD = "bhybszbulvshcwbe"
 
 today = datetime.now()
 today_tuple = (today.month, today.day)
